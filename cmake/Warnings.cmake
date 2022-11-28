bc35bfc6aa72c2db49bad6a2b9e09a9a6d0941d8
franneck94 <schaffranek.jan@web.de>
2022-11-28 08:49:55 +0100

    up

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -14,8 +14,6 @@ function(target_set_warnings)
     set(MSVC_WARNINGS
         /W4 # Baseline reasonable warnings
         /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss of data
-        /w14263 # 'function': member function does not override any base class virtual member function
-        /w14265 # 'classname': class has virtual functions, but destructor is not virtual
         /w14287 # 'operator': unsigned/negative constant mismatch
         /w14296 # 'operator': expression is always 'boolean_value'
         /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
@@ -30,15 +28,11 @@ function(target_set_warnings)
         -Wall
         -Wextra # reasonable and standard
         -Wshadow # warn the user if a variable declaration shadows one from a parent context
-        -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor
-        -Wold-style-cast # warn for c-style casts
         -Wcast-align # warn for potential performance problem casts
         -Wunused # warn on anything being unused
-        -Woverloaded-virtual # warn if you overload (not override) a virtual function
-        -Wpedantic # warn if non-standard C++ is used
+        -Wpedantic # warn if non-standard is used
         -Wconversion # warn on type conversions that may lose data
         -Wnull-dereference # warn if a null dereference is detected
-        -Wdouble-promotion # warn if float is implicit promoted to double
         -Wformat=2 # warn on security issues around functions that format output (ie printf)
     )
 
