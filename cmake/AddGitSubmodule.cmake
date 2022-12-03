e5eaf8e1212ea6fbe2a5bd8c81ec65980b5f9fe0
franneck94 <schaffranek.jan@web.de>
2022-12-03 12:55:18 +0100

    finalized

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -6,6 +6,4 @@ function(add_git_submodule dir)
             COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --
                     ${dir} WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
     endif()
-
-    add_subdirectory(${dir})
 endfunction(add_git_submodule)
